c0fb9c59f77b513e0271a878e0f5f22bf6375803
Akhlakur Rahman <ssrmm2000@gmail.com>
2026-09-24 02:15:20 +0600

    firebase auth established

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,27 @@
-import {
-    createBrowserRouter,
-} from "react-router";
-
+import { createBrowserRouter } from "react-router";
+import Root from "../pages/Root";
+import Error from "../pages/Error";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello World</div>
-    }
+        Component: Root,
+        errorElement: <Error/>,
+        children:[
+            {
+                index: true,
+                Component: Home,
+            },
+            {
+                path: "/login",
+                Component: Login,
+            },
+            {
+                path: "/register",
+                Component: Register,
+            }
+        ]
+    },
 ])
