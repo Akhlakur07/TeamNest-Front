6627b4411c68b4aa93e656838dc7abead3d9fc20
Akhlakur Rahman <ssrmm2000@gmail.com>
2026-09-24 04:56:29 +0600

    payment vars imported

---
+++ src/payment/Payment.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(import.meta.env.VITE_payment_key);
+
+const Payment = () => {
+    return (
+        <Elements stripe={stripePromise}>
+        </Elements>
+    );
+}
+
+export default Payment;
